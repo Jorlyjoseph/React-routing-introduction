b2f19afcab77231cf9f3de6ebb8b31d3bd4e012d
Jorlyjoseph <141339657+Jorlyjoseph@users.noreply.github.com>
2023-09-21 14:36:59 +0200

    react routing intro done

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ErrorPage from "./pages/ErrorPage";
+import HomePageWithNavigate from "./pages/HomePageWithNavigate";
+import projectsData from "./projects-data.json";
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />;
+      {/*   Add <Route /> components between <Routes> and </Routes>   */}
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePageWithNavigate />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/projects"
+          element={<ProjectsPage projects={projectsData} />}
+        />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
